bb47dce1c3d4fdcdedc34370fc784c7e315b35a8
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-18 12:34:26 -0600

    Add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_18_170432) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_18_182326) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
