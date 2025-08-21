0f048e9859bb9ce8f336d94617bac8010c773cc5
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-21 11:08:26 -0600

    Add User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
+
+  def gravatar_id
+    Digest::MD5::hexdigest(email.downcase)
+  end
 end
