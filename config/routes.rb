00a1cc3c30d4d562bb294b57c5091521f45896e4
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-21 11:47:59 -0600

    Add Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   resources :users
   get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
   
   resources :movies do
     resources :reviews
