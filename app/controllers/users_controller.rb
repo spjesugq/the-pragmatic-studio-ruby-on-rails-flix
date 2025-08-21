2d6e1b829cf14545bada3def73f23a33a179f073
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-21 11:25:59 -0600

    Add Edit User Account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,29 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, status: :see_other,
+      alert: "Account successfully deleted!"
+  end
 private
   def user_params
     params.require(:user)
-      .permit(:name, :email, :password, :password_confirmation)
+      .permit(:name, :email, :password, :password_confirmation, :username)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
+  validates :username, presence: true,
+                     format: { with: /\A[A-Z0-9]+\z/i },
+                     uniqueness: { case_sensitive: false }
 
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
