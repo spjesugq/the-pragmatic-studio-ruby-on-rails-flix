ab42902176d0aefcca83232b10151a3eab0d0f51
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-22 12:31:45 -0600

    Add Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+private
+  helper_method :current_user
+  helper_method :current_user?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    current_user == user
+  end
+
+
+  def require_signin
+    unless current_user
+      session[:intended_url] = request.url
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    unless current_user == @user
+      redirect_to root_url, status: :see_other unless current_user?(@user)
+    end
+  end
 end
