c52f213a9b75482c7969f9dd42d0af8b8387288e
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-24 13:02:46 -0600

    Add Through Associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :favorites
+  resources :genres
   root "movies#index"
-
+  
   resources :users
   get "signup" => "users#new"
-
+  
   resource :session, only: [:new, :create, :destroy]
   get "signin" => "sessions#new"
   
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy]
   end
 end
