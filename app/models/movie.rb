645f40fb36b481f556d963686258261df3500638
spjesugq <jesus.gonzalez@simplepractice.com>
2025-08-25 13:25:50 -0600

    Add Active Storage File Uploads

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,17 +8,17 @@ class Movie < ApplicationRecord
   has_many :characterizations, dependent: :destroy
   has_many :genres, through: :characterizations
 
+  has_one_attached :main_image
+
   RATINGS = %w(G PG PG-13 R NC-17)
 
   validates :title, presence: true, uniqueness: true
   validates :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
-  validates :image_file_name, format: {
-    with: /\w+\.(jpg|png)\z/i,
-    message: "must be a JPG or PNG image"
-  }
   validates :rating, inclusion: { in: RATINGS }
+
+  validate :acceptable_image
 
   scope :released, -> { where("released_on < ?", Time.now).order("released_on desc") }
   scope :upcoming, -> { where("released_on > ?", Time.now).order("released_on asc") }
@@ -48,5 +48,18 @@ class Movie < ApplicationRecord
 private
   def set_slug
     self.slug = title.parameterize
+  end
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.blob.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
   end
 end
